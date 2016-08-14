d6dd3db8e14e066f6d03fd3dbeba84e64aba4817
utsushiiro <ozeki.haru@gmail.com>
2016-08-14 23:25:54 +0900

    [add] Routerクラス作成
    ・ルーティング処理を行うRouterクラスを作成
    ・Routerクラスの動作確認用にRouterTest.phpを作成

---
+++ application/core/Router.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace core;
+
+/**
+ * ルーティングを制御するcoreクラス
+ *
+ * @package core
+ */
+class Router
+{
+    /**
+     * このRouterに登録されている{@link Route}集合
+     *
+     * @var Route[]
+     */
+    private $routes;
+
+    /**
+     * パスインフォパターンとそれに対応するルーティングパラメタの対応の集合
+     *
+     * {@link $routes} が {@link compileRoutes} により変換され作られる。
+     * これを利用して {@link resolve} はパスインフォをルーティングパラメタに変換する。
+     *
+     * @var array
+     */
+    private $compiled_routes;
+
+    /**
+     * Router constructor.
+     */
+    public function __construct()
+    {
+        $this->routes = [];
+    }
+
+    /**
+     * 引数で与えられたRouteを$routesに登録する
+     *
+     * @param Route $route
+     * @param bool $replace
+     * @return bool
+     */
+    public function addRoute(Route $route, bool $replace=true) : bool
+    {
+        if (array_key_exists($route->getPathInfo(), $this->routes)):
+            if ($replace):
+                $this->routes[$route->getPathInfo()] = $route->getRoutingParams();
+                return true;
+            else:
+                return false;
+            endif;
+        else:
+            $this->routes[$route->getPathInfo()] = $route->getRoutingParams();
+            return true;
+        endif;
+    }
+
+    /**
+     * 引数で与えられたRouteを$routesから除外する
+     *
+     * @param Route $route
+     * @return bool
+     */
+    public function removeRoute(Route $route) : bool
+    {
+        if (array_key_exists($route->getPathInfo(), $this->routes)):
+            unset($this->routes[$route->getPathInfo()]);
+            return true;
+        else:
+            return false;
+        endif;
+    }
+
+    /**
+     * $routesを元に$compiled_routesを作成する
+     *
+     * $routesに登録されているRouteの$path_infoに含まれる動的パラメタを正規表現に書き換え、
+     * パスインフォパターンを作成する。そして、これを$keyとして連想配列$compiled_routesを作成する。
+     * なお、$valueはRouteの$routing_paramsをそのまま使用する。
+     */
+    public function compileRoutes()
+    {
+        $this->compiled_routes = [];
+
+        // $route から $compiled_routesを作成する
+        foreach ($this->routes as $path_info => $routing_param):
+
+            // $path_infoをセパレータ'/'でトークンに分離する
+            $tokens = explode('/', $path_info);
+
+            // トークンが':'で始まる場合(動的パラメタの場合)、正規表現に置き換える
+            foreach ($tokens as &$token) :
+                if (strpos($token, ':') === 0):
+                    $name = substr($token, 1);
+                    $token = '(?P<' . $name . '>[^/]+)';
+                endif;
+            endforeach;
+
+            // 分離したトークンを再結合する
+            // PCRE正規表現を使用するため'#'で囲む
+            $pattern = "#^". implode('/', $tokens) . '$#';
+            $this->compiled_routes[$pattern] = $routing_param;
+        endforeach;
+    }
+
+    /**
+     * 引数で与えられたパスインフォと$compiled_routesのパスインフォパターンのマッチングを行い、
+     * 対応するルーティングパラメタにマッチングにより得られた動的パラメタの情報を加えた配列を返す。
+     *
+     * @param string $path_info
+     * @return array|null
+     */
+    public function resolve(string $path_info)
+    {
+        // $path_infoの先頭に「/」がない場合は付ける
+        if (substr($path_info, 0, 1) !== '/'):
+            $path_info = '/' . $path_info;
+        endif;
+        
+        foreach ($this->compiled_routes as $pattern => $params):
+            $res = preg_match($pattern, $path_info, $matches);
+            if ($res === 1):
+                    $params = array_merge($params, $matches);
+                    return $params;
+            endif;
+        endforeach;
+
+        return null;
+    }
+
+    /**
+     * [debug] $routesと$compiled_routesのダンプ
+     */
+    public function dump_routes()
+    {
+        echo '---------- $routes ---------', PHP_EOL;
+        var_dump($this->routes);
+        echo '----- $compiled_routes -----', PHP_EOL;
+        var_dump($this->compiled_routes);
+    }
+}
+
+/**
+ * パスインフォとそれに対応するルール(呼び出すコントローラ、アクション)を定義する<br>
+ *
+ * '/user/:id/edit' => array('controller' => 'user', 'action' => 'edit') という対応の場合、
+ * '/user/:id/edit' は $path_info に, array('controller' => 'user', ...) は $routing_params に格納される
+ *
+ * @package core
+ */
+class Route
+{
+    /**
+     * パスインフォ(動的パラメタ含む)<br>
+     *
+     * 動的パラメタとは/user/:id 等の ":"で始まる文字列を指す。
+     *
+     * @var string
+     */
+    private $path_info;
+
+    /**
+     * パスインフォに対応するコントローラとアクション等の情報<br>
+     *
+     * @var string[]
+     */
+    private $routing_params;
+
+    /**
+     * Route constructor.
+     *
+     * @param string $path_info
+     * @param string[] $routing_params
+     */
+    public function __construct(string $path_info, array $routing_params)
+    {
+        $this->path_info = $path_info;
+        $this->routing_params = $routing_params;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPathInfo(): string
+    {
+        return $this->path_info;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getRoutingParams(): array
+    {
+        return $this->routing_params;
+    }
+
+    /**
+     * Routeオブジェクトの等価判定
+     *
+     * $path_infoが同一であるRouteオブジェクトを等価であると定める。
+     *
+     * @param Route $route 比較するRoute
+     * @return bool
+     */
+    public function equals(Route $route) : bool
+    {
+        return $this->path_info === $route->path_info;
+    }
+}
---
+++ application/tests/RouterTest.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once "../bootstrap.php";
+
+$router = new \core\Router();
+
+// ルーティングパラメタのindexについて、主要なもの(コントローラ等)は
+// 文字列ではなく定数(等)で指定できるようにすべきかも
+$route1 = new \core\Route(
+    '/',
+    array('controller' => 'home', 'action' => 'index')
+);
+$route2 = new \core\Route(
+    '/user/:id/edit',
+    array(
+        'controller' => 'user',
+        'action' => 'edit'
+    )
+);
+$route3 = new \core\Route(
+    '/',
+    array('controller' => 'foo', 'action' => 'bar')
+);
+
+
+// addRoute(), compileRoutes()の動作確認
+$router->addRoute($route1);
+$router->addRoute($route2);
+$router->addRoute($route3, false);
+$router->compileRoutes();
+$router->dump_routes();
+
+// $route1のresolve()の動作確認
+$path_info = '/';
+$routing_params = $router->resolve($path_info);
+echo '----- $route1のresolve() ------', PHP_EOL;
+var_dump($routing_params);
+
+// $route2のresolve()の動作確認
+echo '----- $route2のresolve() ------', PHP_EOL;
+$path_info = '/user/2/edit';
+$routing_params = $router->resolve($path_info);
+var_dump($routing_params);
+
+// $route4のresolve()の動作確認
+echo '----- $route4のresolve() ------', PHP_EOL;
+$route4 = new \core\Route(
+    '/user/:id/edit/:article',
+    array(
+        'controller' => 'user',
+        'action' => 'edit'
+    )
+);
+$router->addRoute($route4);
+$router->compileRoutes();
+$router->dump_routes();
+$path_info = '/user/114/edit/514';
+$routing_params = $router->resolve($path_info);
+var_dump($routing_params);
